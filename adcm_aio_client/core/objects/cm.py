90c916e297130a527eb88ffd9f1882c075860e6b
Skrynnik Daniil <d.skrynnik@arenadata.io>
2024-11-19 16:57:25 +0300

    ADCM-6115: Implement Service object

--- adcm_aio_client/core/objects/cm.py
+++ adcm_aio_client/core/objects/cm.py
@@ -139,6 +139,14 @@ class Service(
     def cluster(self: Self) -> Cluster:
         return self._parent
 
+    @property
+    def name(self: Self) -> str:
+        return self._data["name"]
+
+    @property
+    def display_name(self: Self) -> str:
+        return self._data["displayName"]
+
     def get_own_path(self: Self) -> Endpoint:
         return *self._parent.get_own_path(), self.PATH_PREFIX, self.id
 
