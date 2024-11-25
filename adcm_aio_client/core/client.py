0477caec6af762d251d80ed0016e53d7ec517e5b
astarovo <a.starovoitov@arenadata.io>
2024-11-26 02:50:58 +0300

    ADCM-6138: Implement Bundle object

--- adcm_aio_client/core/client.py
+++ adcm_aio_client/core/client.py
@@ -13,9 +13,10 @@
 from functools import cached_property
 from typing import Self
 
-from adcm_aio_client.core.objects.cm import ADCM, ClustersNode, HostProvidersNode, HostsAccessor
-from adcm_aio_client.core.requesters import DefaultRequester
-from adcm_aio_client.core.types import AuthToken, Cert, Credentials, Requester, Verify
+from adcm_aio_client.core.objects.cm import ADCM, ClustersNode, HostProvidersNode
+from adcm_aio_client.core.objects.cm import HostsAccessor
+from adcm_aio_client.core.requesters import Requester
+from adcm_aio_client.core.types import AuthToken, Cert, Credentials, Verify
 
 
 class ADCMClient:
@@ -37,6 +38,10 @@ class ADCMClient:
     @cached_property
     def adcm(self: Self) -> ADCM:
         return ADCM(requester=self._requester, data={})
+
+    @cached_property
+    def bundles(self: Self) -> BundlesNode:
+        return BundlesNode(path=(), requester=self._requester)
 
 
 async def build_client(
