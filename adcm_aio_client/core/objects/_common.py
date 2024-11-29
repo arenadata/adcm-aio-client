ade0a5fa241a5cdae996c7ab3c39344b193ab7e0
Skrynnik Daniil <d.skrynnik@arenadata.io>
2024-11-29 11:51:22 +0300

    ADCM-6134: review fixes

--- adcm_aio_client/core/objects/_common.py
+++ adcm_aio_client/core/objects/_common.py
@@ -19,11 +19,7 @@ class WithStatus(WithRequester, AwareOfOwnPath):
 class WithActions(WithRequester, AwareOfOwnPath):
     @cached_property
     def actions(self: Self) -> ActionsAccessor:
-        return ActionsAccessor(
-            parent=self,
-            path=(*self.get_own_path(), "actions"),
-            requester=self._requester,
-        )
+        return ActionsAccessor(parent=self, path=(*self.get_own_path(), "actions"), requester=self._requester)
 
 
 # todo whole section lacking implementation (and maybe code move is required)
