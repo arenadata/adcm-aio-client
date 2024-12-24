2f443a747cf1ad61a99c51e803ddd537eb6d2636
Egor Araslanov <aer@arenadata.io>
2024-12-24 14:13:01 +0500

    ADCM-6210 missing changes

--- adcm_aio_client/core/actions/_objects.py
+++ adcm_aio_client/core/actions/_objects.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING, Any, Self
 from asyncstdlib import cached_property as async_cached_property
 
 from adcm_aio_client.core.config._objects import ActionConfig
-from adcm_aio_client.core.config.types import ConfigData
+from adcm_aio_client.core.config.types import ActionConfigData
 from adcm_aio_client.core.errors import HostNotInClusterError, NoConfigInActionError, NoMappingInActionError
 from adcm_aio_client.core.filters import FilterByDisplayName, FilterByName, Filtering
 from adcm_aio_client.core.mapping import ActionMapping
@@ -90,7 +90,7 @@ class Action(InteractiveChildObject):
             raise NoConfigInActionError(message)
 
         configuration = self._configuration
-        data = ConfigData.from_v2_response(data_in_v2_format=configuration)
+        data = ActionConfigData(values=configuration["config"], attributes=configuration["adcmMeta"])
         schema = configuration["configSchema"]
 
         return ActionConfig(schema=schema, config=data, parent=self)
