3f4a69c1403888c4bd2f37fa996d0b360885a124
Araslanov Egor <aer@arenadata.io>
2025-01-17 18:49:35 +0500

    ADCM-6277 Change diff calculation algorithm (#75)
    Co-authored-by: Araslanov Egor <aer@slwg.local>

--- adcm_aio_client/config/_operations.py
+++ adcm_aio_client/config/_operations.py
@@ -1,56 +1,35 @@
-from adcm_aio_client.config._types import (
-    ConfigSchema,
-    FullConfigDifference,
-    GenericConfigData,
-    LevelNames,
-    ValueChange,
-    full_name_to_level_names,
-)
+from contextlib import suppress
+
+from adcm_aio_client.config._types import ConfigSchema, GenericConfigData, LevelNames, ParameterChange
 
 
 # Difference
 def find_config_difference(
     previous: GenericConfigData, current: GenericConfigData, schema: ConfigSchema
-) -> FullConfigDifference:
-    diff = FullConfigDifference(schema=schema)
+) -> dict[LevelNames, ParameterChange]:
+    diff = {}
 
-    _fill_values_diff_at_level(level=(), diff=diff, previous=previous.values, current=current.values)
-    _fill_attributes_diff(diff=diff, previous=previous.attributes, current=current.attributes)
+    for names, _ in schema.iterate_parameters():
+        prev = {"value": None, "attrs": {}}
+        cur = {"value": None, "attrs": {}}
+
+        # TypeError / KeyError may occur when `None` is in values
+        # (e.g. structure with dict as root item and with None value)
+        if not schema.is_group(names):
+            with suppress(TypeError, KeyError):
+                prev["value"] = previous.get_value(names)
+
+            with suppress(TypeError, KeyError):
+                cur["value"] = current.get_value(names)
+        else:
+            prev.pop("value")
+            cur.pop("value")
+
+        attr_key = f"/{'/'.join(names)}"
+        prev["attrs"] = previous.attributes.get(attr_key, {})
+        cur["attrs"] = current.attributes.get(attr_key, {})
+
+        if prev != cur:
+            diff[names] = ParameterChange(previous=prev, current=cur)
 
     return diff
-
-
-def _fill_values_diff_at_level(level: LevelNames, diff: FullConfigDifference, previous: dict, current: dict) -> None:
-    missing = object()
-    for key, cur_value in current.items():
-        level_names = (*level, key)
-        prev_value = previous.get(key, missing)
-
-        if prev_value is missing:
-            # there may be collision between two None's, but for now we'll consider it a "special case"
-            diff.values[level_names] = ValueChange(previous=None, current=cur_value)
-            continue
-
-        if cur_value == prev_value:
-            continue
-
-        if not (diff.schema.is_group(level_names) and isinstance(prev_value, dict) and (isinstance(cur_value, dict))):
-            diff.values[level_names] = ValueChange(previous=prev_value, current=cur_value)
-            continue
-
-        _fill_values_diff_at_level(diff=diff, level=level_names, previous=prev_value, current=cur_value)
-
-
-def _fill_attributes_diff(diff: FullConfigDifference, previous: dict, current: dict) -> None:
-    missing = object()
-    for full_name, cur_value in current.items():
-        prev_value = previous.get(full_name, missing)
-        if cur_value == prev_value:
-            continue
-
-        level_names = full_name_to_level_names(full_name)
-
-        if prev_value is missing:
-            prev_value = None
-
-        diff.attributes[level_names] = ValueChange(previous=prev_value, current=cur_value)
