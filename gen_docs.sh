a0e9f6512e4adae9285e3355ccb54a00ac65d6c7
Araslanov Egor <aer@arenadata.io>
2025-02-19 16:53:39 +0500

    ADCM-6367 Infra for "stupid" sphinx doc generation

--- gen_docs.sh
+++ gen_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-poetry run sphinx-apidoc --ext-viewcode --ext-autodoc --separate --private -o docs/api adcm_aio_client
+poetry run sphinx-apidoc --ext-viewcode --ext-autodoc --separate --private --module-first -o docs/api adcm_aio_client
 poetry run sphinx-build docs docs-out
