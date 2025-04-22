02c65ef5ec392f985ce127f4146bd92bf3e43b30
Artem Starovoitov <arteom.starovoitov@yandex.ru>
2025-04-22 09:49:34 +0300

    ADCM-6497: Implement API docs for ADCMClient, ADCMSession and Mapping (#106)

--- adcm_aio_client/_session.py
+++ adcm_aio_client/_session.py
@@ -26,6 +26,17 @@ MIN_ADCM_VERSION = "2.5.0"
 
 
 class ADCMSession:
+    """
+    Context manager providing building of adcm object session running at `url` with `credentials`. Within context,
+    `ADCMClient` is available for further usage.
+
+    ```python
+        creds = Credentials("admin", "admin")
+        async with ADCMSession(creds) as client:
+            ...
+    ```
+    """
+
     def __init__(
         self: Self,
         # basics
@@ -54,6 +65,9 @@ class ADCMSession:
     # Context Manager
 
     async def __aenter__(self: Self) -> ADCMClient:
+        """
+        Constructs ADCMClient object for session running at `url` with `credentials`.
+        """
         self._http_client = await self._prepare_http_client_for_running_adcm()
         adcm_version_ = await _ensure_adcm_version_is_supported(client=self._http_client)
 
@@ -73,6 +87,9 @@ class ADCMSession:
         exc_value: BaseException | None = None,
         traceback: TracebackType | None = None,
     ) -> None:
+        """
+        Closes ADCM session.
+        """
         await self.__close_requester_safe(exc_type, exc_value, traceback)
         await self.__close_http_client_safe(exc_type, exc_value, traceback)
 
