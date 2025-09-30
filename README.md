01ccd9a51e875b3935790d71d48459b29cd45a09
Aleksandr Alferov <aas@arenadata.io>
2025-09-30 10:55:11 +0300

    Remove tests for upgrade with hc_acl (#108)

--- README.md
+++ README.md
@@ -119,5 +119,5 @@ poetry run pytest tests/unit
 To run the integration tests:
 
 ```shell
-poetry run pytest/integration
+poetry run pytest tests/integration
 ```
--- tests/integration/test_upgrade.py
+++ tests/integration/test_upgrade.py
@@ -12,7 +12,6 @@
 
 from pathlib import Path
 from typing import NamedTuple
-import asyncio
 
 import yaml
 import pytest
@@ -25,9 +24,7 @@ from adcm_aio_client.errors import (
     ConfigNoParameterError,
     NoConfigInActionError,
     NoMappingInActionError,
-    UnknownError,
 )
-from adcm_aio_client.mapping import apply_remote_changes
 from adcm_aio_client.objects import Bundle, HostProvider, Job
 from tests.integration.bundle import pack_bundle
 
@@ -89,7 +86,6 @@ def context(
 async def test_upgrade_api(context: Context, tmp_path: Path) -> None:
     await _test_simple_upgrade(context)
     await _test_upgrade_with_config(context)
-    await _test_upgrade_with_mapping(context)
     await _test_upgrade_filtering(context, tmp_path)
 
 
@@ -134,7 +130,7 @@ async def _test_upgrade_with_config(context: Context) -> None:
     cluster = await context.client.clusters.create(context.old_bundle, "for config")
 
     upgrades = await cluster.upgrades.filter(display_name__contains="With action")
-    assert len(upgrades) == 2
+    assert len(upgrades) == 1
     upgrade = await cluster.upgrades.get_or_none(display_name__in=["With action and config"])
     assert upgrade is not None
 
@@ -168,69 +164,6 @@ async def _test_upgrade_with_config(context: Context) -> None:
     await job.wait(timeout=30)
     await cluster.refresh()
     assert (await cluster.bundle).id == context.new_bundle.id
-
-
-async def _test_upgrade_with_mapping(context: Context) -> None:
-    cluster = await context.client.clusters.create(context.old_bundle, "for mapping")
-    await asyncio.gather(
-        *(
-            context.client.hosts.create(hostprovider=context.hostprovider, name=f"host-{i}", cluster=cluster)
-            for i in range(3)
-        )
-    )
-    service_1, *_ = await cluster.services.add(Filter(attr="name", op="eq", value="example_1"))
-    service_2, *_ = await cluster.services.add(Filter(attr="name", op="eq", value="example_2"))
-
-    # todo most likely bug
-    #    upgrades = await cluster.upgrades.filter(
-    # display_name__iexclude=["With action and config", "i am very simple, you know?"])
-    #    assert len(upgrades) == 1, [u.display_name for u in upgrades]
-    #    upgrade = upgrades[0]
-    upgrade = await cluster.upgrades.get(name__eq="action_config_mapping")
-    assert upgrade.name == "action_config_mapping"
-
-    mapping = await upgrade.mapping
-    assert mapping.all() == []
-
-    assert len(await cluster.hosts.all()) == 3
-    cluster_mapping = await cluster.mapping
-    assert cluster_mapping.all() == []
-    await cluster_mapping.add(
-        component=await service_1.components.get(name__eq="second"),
-        host=Filter(attr="name", op="contains", value="host"),
-    )
-    second_c_service_2 = await service_2.components.get(name__eq="second")
-    await cluster_mapping.add(component=second_c_service_2, host=Filter(attr="name", op="ieq", value="HOsT-1"))
-    assert len(cluster_mapping.all()) == 4
-    await cluster_mapping.save()
-
-    await upgrade.refresh()  # drop caches
-    mapping = await upgrade.mapping
-    assert len(mapping.all()) == 4
-    await mapping.add(
-        component=await service_1.components.get(display_name__eq="First Component"),
-        host=await mapping.hosts.filter(name__in=["host-0", "host-2"]),
-    )
-    assert len(mapping.all()) == 6
-    await mapping.remove(component=second_c_service_2, host=await mapping.hosts.get(name__eq="host-1"))
-    assert len(mapping.all()) == 5
-
-    config = await upgrade.config
-    config["params", ParameterGroup]["pass", Parameter].set("notenough")
-
-    # quite strange pick of response in here in ADCM, so generalized expected error
-    with pytest.raises(UnknownError, match=".*config key.*is required"):
-        await upgrade.run()
-
-    config["string_field", Parameter].set("useless yet required")
-
-    job = await upgrade.run()
-    assert isinstance(job, Job)
-    await job.wait(timeout=30, poll_interval=2)
-    assert await job.get_status() == "success"
-
-    await cluster_mapping.refresh(strategy=apply_remote_changes)
-    assert len(cluster_mapping.all()) == 5
 
 
 async def _test_upgrade_filtering(context: Context, tmp_path: Path) -> None:
