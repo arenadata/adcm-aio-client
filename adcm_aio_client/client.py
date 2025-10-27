f8aa4e896b25e6598625919a26c317f7579287d3
Skrynnik Daniil <d.skrynnik@arenadata.io>
2025-10-27 17:25:01 +0300

    ADCM-7232: remove unused import

--- adcm_aio_client/client.py
+++ adcm_aio_client/client.py
@@ -18,7 +18,6 @@ from adcm_aio_client.objects import ADCM
 from adcm_aio_client.objects._cm import BundlesNode, ClustersNode, HostProvidersNode, HostsNode, JobsNode
 from adcm_aio_client.objects._rbac import GroupsNode, PoliciesNode, RolesNode  # , UsersNode
 
-# from adcm_aio_client.objects._rbac_new_lazy import UsersNode
 from adcm_aio_client.objects.rbac._user import UsersNode
 from adcm_aio_client.requesters import BundleRetrieverInterface, Requester
 
