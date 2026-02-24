8cbeb9c24c96688e5fbf6b6447dcc0cf795469bb
Skrynnik Daniil <d.skrynnik@arenadata.io>
2026-02-24 13:15:44 +0300

    Revert "ADCM-7325: commit to trigger pipeline"
    This reverts commit 30842983d0626c7665d387f6f542fad01ccb49b4.

--- tests/integration/test_job.py
+++ tests/integration/test_job.py
@@ -112,7 +112,7 @@ async def _test_basic_api(adcm_client: ADCMClient) -> None:
     assert job.start_time is None
     assert job.finish_time is None
     assert (await job.action).id == action.id
- # comment
+
     await job.wait(exit_condition=is_running, timeout=60, poll_interval=3)
     assert job.start_time is None
     await job.refresh()
