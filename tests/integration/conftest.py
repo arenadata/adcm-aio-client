b2fc89f52e5ed63b346f1395c43cb0ca660c1051
Aleksandr Alferov <aas@arenadata.io>
2026-09-07 15:33:47 +0300

    Revert "Disable `wait_for_ssl` (test CI run)"
    This reverts commit b946bc08bc0917ce905e73f227d525aa69dabf45.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -130,7 +130,7 @@ def adcm(network: Network, postgres: ADCMPostgresContainer, adcm_image: str) -> 
     db = DatabaseInfo(name=f"adcm_{suffix}", host=postgres.name)
     postgres.execute_statement(f"CREATE DATABASE {db.name} OWNER {DB_USER}")
 
-    adcm = ADCMContainer(image=adcm_image, network=network, db=db, wait_for_ssl=False)
+    adcm = ADCMContainer(image=adcm_image, network=network, db=db)
 
     with adcm as container:
         yield container
